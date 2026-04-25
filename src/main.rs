d9f454e972ad9ead1f2d698280f6832f22c5363e
Michael <HighiqMike1@gmail.com>
2026-04-25 16:26:26 -0700

    made it to where you can thow the ball while holding the ball

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::option;
+
 use macroquad::prelude::*;
 #[macroquad::main("bountingball")]
 async fn main() {
@@ -6,6 +8,10 @@ async fn main() {
     let gravity = 0.5;
     let mut yvelotiy = 1.0;
     let mut xvelotiy = 1.0;
+    let mut xoldmousepos = 1.0;
+    let mut yoldmousepos = 1.0;
+    let mut xnewmousepos = 1.0;
+    let mut ynewmousepos = 1.0;
     loop{
     
 
@@ -13,9 +19,15 @@ async fn main() {
     
     if  is_mouse_button_down(MouseButton::Left){
         (x,y) = mouse_position();
-      
+        (xoldmousepos,yoldmousepos) = mouse_position();
         
+        
+    }else if is_mouse_button_released(MouseButton::Left) {
+    (xnewmousepos,ynewmousepos) = mouse_position();
+    xvelotiy = (xnewmousepos).abs() - (xoldmousepos).abs();
+    yvelotiy = (ynewmousepos).abs() - (yoldmousepos).abs();
     }else {
+    
     y = y + yvelotiy;
     x = x +xvelotiy;
     yvelotiy = yvelotiy + gravity;
